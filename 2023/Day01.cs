161277ff78402e43f82c29694d3b5f21977528f6
Austin Stanley <MaxTMahem@gmail.com>
2023-12-01 17:54:22 -0600

    cleaned up comments mainly

--- 2023/Day01.cs
+++ 2023/Day01.cs
@@ -1,40 +1,27 @@
-﻿namespace AdventOfCode;
+﻿namespace AdventOfCodeMax._2023;
 
 using AdventOfCodeSupport;
-using Iced.Intel;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
 
 public class Day01 : AdventBase
 {
-    protected override void InternalOnLoad() {
-        // Optional override, runs before Part1/2.
-        // Benchmarked separately.
-    }
-
     protected override object InternalPart1() {
         var enumerator = Input.Text.GetEnumerator();
         int sum = 0;
         int resultDigit;
 
-        // this while will step through all "lines". If empty will just fall through.
         do {
-            // find the first digit.
-            resultDigit = GetNextDigit(enumerator);
+            int firstDigit = GetNextDigit(enumerator);
 
-            // AoC code does not appear to have any cases with *no* digits in a line,
-            // but we'll handle that case just in case.
-            if (resultDigit == 0) continue;
+            // AoC code does not appear to have any cases with *no* digits in a line, but handle that anyways.
+            if (firstDigit == 0) continue;
 
-            // if there are no more digits in the line, then the first digit is the second digit.
-            // set the first digit as the default value.
-            int firstDigit  = resultDigit;
-            int secondDigit = resultDigit;
+            // secondDigit is firstDigit if no other digit is found.
+            int secondDigit = firstDigit;
 
-            // this while will step through until the end of line or end of enumeration, not just the next digit.
-            // multiple digits may be found, we only care about the last one.
+            // step through all remaining digits until end of line
             while ((resultDigit = GetNextDigit(enumerator)) != 0) {
-                // if a digit is found, update the second digit. If no digit found, keep the same digit.
+                // if a new digit is found, it becomes the second digit, otherwise we keep the old one.
                 secondDigit = resultDigit != 0 ? resultDigit : secondDigit;
             }
 
@@ -42,7 +29,7 @@ public class Day01 : AdventBase
         } while (enumerator.MoveNext());
         return sum;
 
-        // Gets the Next Digit, or advances to the next line, or end of enumeration.
+        // Gets the Next Digit, or 0 if EoL.
         static int GetNextDigit(CharEnumerator charEnumerator) {
             while (charEnumerator.MoveNext() && charEnumerator.Current != '\r') {
                 if (char.IsAsciiDigit(charEnumerator.Current)) return charEnumerator.Current - '0';
@@ -55,29 +42,21 @@ public class Day01 : AdventBase
         var enumerator = Input.Text.GetEnumerator();
 
         // wordsMatchedLetters keeps track of how many matching letters have been seen in every word
-        // for example, if the letters 'eigh' have been seen, then the 7th value with have a value of 4
-        // meaning the 5th index should be checked next
         Span<int> wordsMatchedLetters = stackalloc int[WordNumbers.Length];
         int sum = 0;
         int resultDigit;
 
-        // this while will step through all "lines". If empty will just fall through.
         do {
-            // find the first digit.
-            resultDigit = GetNextDigit2(enumerator, wordsMatchedLetters);
+            int firstDigit = GetNextDigit(enumerator, wordsMatchedLetters);
 
-            // AoC code does not appear to have any cases with *no* digits in a line,
-            // but we'll handle that case just in case.
-            if (resultDigit == 0) continue;
+            // AoC code does not appear to have any cases with *no* digits in a line, but handle that anyways.
+            if (firstDigit == 0) continue;
 
-            // if there are no more digits in the line, then the first digit is the second digit.
-            // set the first digit as the default value.
-            int firstDigit  = resultDigit;
-            int secondDigit = resultDigit;
+            // secondDigit is firstDigit if no other digit is found.
+            int secondDigit = firstDigit;
 
-            // this while will step through until the end of line or end of enumeration, not just the next digit.
-            // multiple digits may be found, we only care about the last one.
-            while ((resultDigit = GetNextDigit2(enumerator, wordsMatchedLetters)) != 0) {
+            // step through all remaining digits until end of line
+            while ((resultDigit = GetNextDigit(enumerator, wordsMatchedLetters)) != 0) {
                 // if a digit is found, update the second digit. If no digit found, keep the same digit.
                 secondDigit = resultDigit != 0 ? resultDigit : secondDigit;
             }
@@ -88,15 +67,14 @@ public class Day01 : AdventBase
         return sum;
     }
 
-    private static int GetNextDigit2(CharEnumerator charEnumerator, Span<int> wordsMatchedLetters) {
+    // Gets the Next Digit, or 0 if EoL.
+    private static int GetNextDigit(CharEnumerator charEnumerator, Span<int> wordsMatchedLetters) {
         var resultDigit = 0;
 
         while (resultDigit == 0 && charEnumerator.MoveNext()) {
             char character = charEnumerator.Current;
-            // handle number digit case.
-            if (char.IsAsciiDigit(character)) return charEnumerator.Current - '0';
 
-            // handle EOL character
+            if (char.IsAsciiDigit(character)) return charEnumerator.Current - '0';
             if (character == '\r') return 0;
 
             // letter case. Itterate through all words in our map.
@@ -106,9 +84,7 @@ public class Day01 : AdventBase
                 // get how far into the current word a candiate character should match
                 int matchedLetters = wordsMatchedLetters[wordIndex];
 
-                // check if the current character matches
                 if (word[matchedLetters] == character) {
-                    // increase the matched letter count for that word
                     wordsMatchedLetters[wordIndex]++;
 
                     // if the word has a number of letters matched equal to the word length, its a digit.
@@ -123,14 +99,14 @@ public class Day01 : AdventBase
                 // if the current letter didn't match, reset the matched letter count for that word.
                 else {
                     wordsMatchedLetters[wordIndex] = 0;
-                    // check again because the first letter could repeat
+
+                    // check again for matches because the first letter could repeat
                     matchedLetters = 0;
                     if (word[matchedLetters] == character)
                         wordsMatchedLetters[wordIndex]++;
                 }
             }
         }
-        // case for end of enumeration.
         return resultDigit;
     }
 
--- Program.cs
+++ Program.cs
@@ -2,12 +2,9 @@
 
 var solutions = new AdventSolutions();
 var today = solutions.GetDay(2023, 1);
-// today.Part1().Part2();
+today.Part1().Part2();
 
-await today.Part1().CheckPart1Async();
-await today.Part2().CheckPart2Async();
-
-//today.SetTestInput("oneight");
-//today.SetTestInput("nine75eight");
+//await today.Part1().CheckPart1Async();
+//await today.Part2().CheckPart2Async();
 
 // today.Benchmark();
